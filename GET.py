219d6ab4c6aed5119e46403b1c72c39f888b1685
JakubBlacha <114864986+JakubBlacha@users.noreply.github.com>
2023-05-07 12:08:59 +0200

    Python poprawka

--- GET.py
+++ GET.py
@@ -1,4 +1,10 @@
 import requests
-def download():
-    pobrane = requests.get("https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json")
-    return pobrane
+
+    url ="https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
+
+try:
+    r = requests.head("https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json")
+    print(r.status_code)
+
+except requests.ConnectionError:
+    print("failed to connect")
