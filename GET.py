5d9809976bc66a70c036a1d15732b024e0f5c84b
JakubBlacha <114864986+JakubBlacha@users.noreply.github.com>
2023-05-20 22:29:17 +0200

    Update GET.py

--- GET.py
+++ GET.py
@@ -1,10 +1,29 @@
+#!/usr/bin/python3
+import urllib
+
 import requests
 
-    url ="https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
+# url = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
+url = "https://httpbin.org/post"
 
-try:
-    r = requests.head("https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json")
-    print(r.status_code)
 
-except requests.ConnectionError:
-    print("failed to connect")
+def checkHTTP(response_code):
+    if 400 <= response_code < 600:
+        raise Exception("error")
+
+
+def HTTP_GET(url_link):
+    r = requests.get(url_link)
+    checkHTTP(r.status_code)
+    return r.json()
+
+
+def HTTP_POST(url_link):
+    body = {"name": "natalia"}
+    request = requests.post(url_link, json=body)
+    checkHTTP(request.status_code)
+    return request.json()
+
+
+if __name__ == '__main__':
+    print(HTTP_POST("https://httpbin.org/post"))
